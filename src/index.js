bf83930ea461a3b7fa99c61b2ba03a528394e3bc
Frost0x <kev987@ya.ru>
2019-09-14 21:10:00 +0500

    Add support ie11

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'react-app-polyfill/ie9'
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from "react-redux"
@@ -5,7 +8,6 @@ import { Provider } from "react-redux"
 import store from './store/'
 import { GlobalStyle } from './styles/global.style'
 import RouteHandler from "./router/";
-
 
 const App = () => (
     <Provider store={store}>
